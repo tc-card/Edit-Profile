f9db07c14cfba04ce92d28325cad4b9292f2e776
Ali <alouloucheikh05@gmail.com>
2025-05-21 23:03:13 +0100

    tt

--- Assets/js/profile.js
+++ Assets/js/profile.js
@@ -217,17 +217,6 @@ async function handleSaveProfile(e) {
     }
   }
 }
-
-function isValidUrl(string) {
-  try {
-    new URL(string);
-    return true;
-  } catch (_) {
-    return false;
-  }
-}
-
-
 function isValidUrl(string) {
   try {
     new URL(string);
